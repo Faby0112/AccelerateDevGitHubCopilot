6ed249c56fa986ac5242d7563cc76ebb31a079ba
Faby0112 <fabiolac.a@hotmail.com>
2024-10-30 00:54:20 -0600

    .

--- src/Library.Console/Program.cs
+++ src/Library.Console/Program.cs
@@ -15,6 +15,7 @@ services.AddSingleton<IConfiguration>(configuration);
 
 services.AddScoped<IPatronRepository, JsonPatronRepository>();
 services.AddScoped<ILoanRepository, JsonLoanRepository>();
+services.AddScoped<JsonLoanRepository>();
 services.AddScoped<ILoanService, LoanService>();
 services.AddScoped<IPatronService, PatronService>();
 
